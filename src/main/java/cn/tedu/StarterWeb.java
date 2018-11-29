fd3bf3bbf12898db80f98ebf06f8d538646daa9e
wujinzhi1994 <944215172@qq.com>
2018-11-29 14:46:39 +0800

    wu5.0

--- src/main/java/cn/tedu/StarterWeb.java
+++ src/main/java/cn/tedu/StarterWeb.java
@@ -2,8 +2,10 @@ package cn.tedu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60*30)
 public class StarterWeb {
 	
 	public static void main(String[] args) {
